6879d99aa753d938b11bcc0cca34c624ecf21975
Sabarna Bhowmik <81832529+sabarnait24@users.noreply.github.com>
2023-03-14 17:33:26 +0530

    deploy3

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "" }),
   endpoints: (builder) => ({
     getTransaction: builder.query({
       query: () => "/api/transactions",
