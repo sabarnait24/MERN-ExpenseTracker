ff62fefbcbc6f368d18e69b063f5bfb2d7ecd4c4
Sabarna Bhowmik <81832529+sabarnait24@users.noreply.github.com>
2023-03-14 14:02:24 +0530

    deploy1

--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ app.use(express.json());
 
 app.use("/", require("./routes/routes"));
 
+if (process.env.NODE_ENV === "production") {
+    console.log("In production stage");
+    app.use(express.static(path.resolve(__dirname, "client", "build")))
+    app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+  }
+
 app.listen(port,()=>{
     console.log(`connection is successful at ${port}`);
 }) 
